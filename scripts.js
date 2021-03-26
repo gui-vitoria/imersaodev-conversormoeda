72d6a9b5816885ccc2f0ab762fd95001bb94f70a
Guilherme Lordêlo Seixas Vitória <61258019+gui-vitoria@users.noreply.github.com>
2021-03-26 15:14:38 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var produto = prompt("Qual o produto ou ** que você quer converter para real?")
+
+alert("Hmm, boa pedida. Será que o real ta tão ruim que é melhor pagar em dólar???")
+
+alert("Vamos converter e descobrir!! ")
+
+var valorEmDolarNumero = parseFloat(prompt("Qual o valor em dólar que você quer converter? US$"))
+
+var valorEmReal = valorEmDolarNumero * 5.73
+var valorEmRealFixado = valorEmReal.toFixed(2)
+
+alert("O valor em real será " + "R$ " + valorEmRealFixado)
+
+prompt("Valeu a pena o valor?")
